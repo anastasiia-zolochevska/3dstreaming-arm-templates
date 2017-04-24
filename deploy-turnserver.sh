a4155fa2297e08249c2d1820f63b1f3852d09d25
Anastasiia Zolochevska <anastasiia.zolochevska@gmail.com>
2017-04-24 12:10:27 -0700

    Dockerfile

--- deploy-turnserver.sh
+++ deploy-turnserver.sh
@@ -1,7 +1,4 @@
-echo 'deb http://ftp.us.debian.org/debian jessie main' | tee --append /etc/apt/sources.list
-apt install aptitude
-aptitude install coturn
-internalIp="$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')"
+internalIp="$(ip a | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')"
 externalIp="$(dig +short myip.opendns.com @resolver1.opendns.com)"
 
 echo "listening-port=3478
@@ -17,7 +14,6 @@ server-name=3dstreamingturnubuntu.com
 
 lt-cred-mech
 userdb=/var/lib/turn/turndb
-
 # use real-valid certificate/privatekey files
 cert=/etc/ssl/certificate.pem
 pkey=/etc/ssl/private.key
@@ -26,5 +22,6 @@ no-stdout-log"  | tee /etc/turnserver.conf
 
 turnadmin -a -u user -p 3Dstreaming0317 -r 3dstreamingturnubuntu.com
 
-sudo turnserver
+turnserver
 
+echo "TURN server running. IP: "$externalIp" Username: user, password: 3Dstreaming0317"
